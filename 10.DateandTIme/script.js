ca7aab032438c6d672d9b1e528e9c7af7c4f2454
Isfar Supayan Sazid <isfarsupayansazidpro25@gmail.com>
2024-06-16 20:39:49 +0000

    Date and Time

---
+++ 10.DateandTIme/script.js
@@ -0,0 +1,37 @@
+// let date = new Date();
+// console.log(date.getTime());
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+
+
+// let today = new Date(2023,5,17,2,13,58);
+// console.log(today.toLocaleString());
+
+
+// let myTime = Date.now();
+// console.log(Math.floor(Date.now()/1000));
+
+
+// # get ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// let date = new Date();
+// console.log(date.getDate());
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDay());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getMilliseconds());
+// console.log(date.getTimezoneOffset());
+
+
+/* let date = Date.now()
+let newTime = new Date(date);
+let localTIme = newTime.toLocaleString()
+console.log(localTIme); */
+
+
+// # set +++++++++++++++++++++++++++++++++++++++++++++++++++
