e3e156c50292d73b6315a226742e8b700d53cd33
Isfar Supayan Sazid <isfarsupayansazidpro25@gmail.com>
2024-06-16 07:01:19 +0000

    Datatype Conversion

---
+++ 3.DatatypeConversion/script.js
@@ -0,0 +1,34 @@
+let Input = 33;
+let InputNumber = Number(Input);
+console.log(Input);
+// console.log(typeof Input);
+// console.log(InputNumber);
+// console.log(typeof InputNumber);
+
+// "33" => 33;
+// "33abc" => NaN;
+// "Sazid" => NaN;
+// true => 1; false => 0;
+// null => 0;
+// undefined => NaN;
+
+
+
+let age = 22;
+let StringNumber = String(age)
+// console.log(age);
+// console.log(typeof age);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// 1 => true ; 0 => false;
+// "sazid" => true; 
+// "" => false;
