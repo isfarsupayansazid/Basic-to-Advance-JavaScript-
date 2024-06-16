566483eb77e3b8c22ba2f3abea5f006cddca66aa
Isfar Supayan Sazid <isfarsupayansazidpro25@gmail.com>
2024-06-16 18:08:33 +0000

    All String prototype

---
+++ 8.String/script.js
@@ -0,0 +1,84 @@
+// # String all prototype 
+
+// 1. charAt
+// let str = "Hello Javascript";
+// console.log(str.charAt(4)); // result : o;
+
+// 2. charCodeAt
+// let str = "Hello Javascript";
+// console.log(str.charCodeAt(4)); // result : 111;
+
+// 3.concat
+// let str1 = "Hello";
+// let str2 = "World";
+// console.log(str1.concat(" ", str2)); result : Hello World 
+
+// 4. includes
+// let str = "Hello Javascript";
+// console.log(str.includes("H")); //result : true
+
+// 5. indexOf;
+// let str = "Hello Javascript";
+// console.log(str.indexOf("o")); result : 4
+
+// 6. LastindexOf
+// let str = "Hello Javascript";
+// console.log(str.lastIndexOf("a")); result : 9;
+
+// 7. LocalCompare
+// let str1 = "Hello";
+// let str2 = "Banana";
+// console.log(str1.localeCompare(str2)); result : 1;
+
+// 8. match
+// let str = "Hello Javascript";
+// console.log(str.match(/o/g)); result : 'o'
+
+// 9. repeat 
+// let str = "hello ";
+// console.log(str.repeat(5)); result : hello hello hello hello hello 
+
+// 10. replace 
+// let str = "Welcome to Javascript";
+// console.log(str.replace("Javascript","There")); 
+
+// 11. replaceALl
+// let str = "hello world.the world is";
+// console.log(str.replaceAll("world","universe"));
+
+
+// 12. Length 
+// let str = "Hello Javascript";
+// console.log(str.length); result : 16
+
+// 13.padend
+// let str = "hello";
+// console.log(str.padEnd(10,".")); // result : hello.....
+
+// 14.search
+// let str = "Hello Javascript";
+// console.log(str.search("Hello"));
+
+// 15. slice
+// let str = "Hello Javascript";
+// console.log(str.slice(0,4));
+
+// 16.substring
+// let str2 = "Hello Javascript";
+// console.log(str2.substring(0,4));
+
+// 17.toLowerCase
+// let str = "HELLO JAVASCRIPt";
+// console.log(str.toLowerCase());
+
+// 18.toUpperCase
+// let str = "hello javascript";
+// console.log(str.toUpperCase());
+
+// 19.trim() trim start() trim end();
+// let str = "  Hello javascript  ";
+// console.log(str.trim());
+
+// 20. toString
+// let str = new String(22);
+// console.log(typeof str.toString());
