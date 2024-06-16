eb5c9781958b10edc2e1afce363f7b4cea11db55
Isfar Supayan Sazid <isfarsupayansazidpro25@gmail.com>
2024-06-16 19:45:53 +0000

    Number and Math

---
+++ 9.NumberandMath/script.js
@@ -0,0 +1,27 @@
+// # Number 
+// let score  =1000000;
+// console.log(score.toFixed(2));;
+// console.log(score.toExponential(2));
+// console.log(score.toPrecision(5));
+// console.log(score.toLocaleString('en-BN'));
+// console.log(score.toString().length);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// # Math 
+// console.log(Math.abs(-8));
+// console.log(Math.ceil(5.5));
+// console.log(Math.floor(5.6));
+// console.log(Math.round(5.5));
+// console.log(Math.acos(1));;
+// console.log(Math.asin(0));
+// console.log(Math.PI * 3);
+// console.log(Math.cos(Math.PI/3));
+// console.log(Math.floor(5.9));
+// console.log(Math.max(56,8,5,8));
+// console.log(Math.min(512,82,6));
+// console.log(Math.pow(2,3));
+// console.log(Math.sqrt(25));
+console.log(Math.round(5.8));
+console.log(Math.floor(Math.random() * 5) + 5);
