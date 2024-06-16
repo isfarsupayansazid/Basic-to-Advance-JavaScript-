838dd5b9c0de0c108f1aef3c32d268c02eb4b360
Isfar Supayan Sazid <isfarsupayansazidpro25@gmail.com>
2024-06-16 06:12:29 +0000

    DataTypes in JS

---
+++ 2.Datatypes/datatypes.js
@@ -0,0 +1,27 @@
+// Data Types in Javascript 
+
+// 1. String ("")
+let Name = "Isfar Supayan Sazid";
+console.log(typeof Name);
+
+// 2. Number (22)
+let age = 22;
+console.log(typeof age);;
+
+// 3. Boolean (true/false)
+let permission = true;   
+console.log(typeof permission);
+
+// 4. Bigint (big number);
+let bigNumber = 1352165126354165n;
+console.log(typeof bigNumber);
+
+// 5. undefined 
+let gap;
+console.log(typeof gap);
+
+// 6. null
+let temperature = null;
+console.log(typeof temperature);
+
+// 7. Symbol => use for uniqueness
