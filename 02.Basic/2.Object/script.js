ef93af0d521ab8533d74390db288ecf0c3f737ef
Isfar Supayan Sazid <isfarsupayansazidpro25@gmail.com>
2024-06-17 17:04:31 +0000

    Object

--- 02.Basic/2.Object/script.js
+++ 02.Basic/2.Object/script.js
@@ -28,3 +28,52 @@
 console.log(JSuser.greeting()); */
 
 
+
+// ++++++++++++++++++++++++Part two +++++++++++++++++++++++++++++++++++
+const trinder = {};
+trinder.id = 123;
+trinder.name = "isfar";
+trinder.isLoggedIn = true;
+console.log(trinder);
+// console.log(Object.keys(trinder));
+// console.log(Object.values(trinder));
+// console.log(Object.entries(trinder));
+// console.log(Object.hasOwnProperty('isLoggedIn'));
+
+const regularUser = {
+    userInformation : {
+        id : "123abc",
+        name : {
+            firstName : "isfar",
+            lastName : "supayan"
+        }
+    }
+}
+// console.log(regularUser.userInformation.name.firstName);
+
+
+const obj1 = {  
+    1 : "a",
+    2 : "b"    
+}
+const obj2 = {
+    3 : "c",
+    4 : "d"
+}
+// const obj3 = {obj1,obj2};
+// const obj3 = Object.assign({},obj1,obj2);
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+const User = [
+    {
+        name : "sazid",
+        email : "is@gmail.com"
+    },
+    {
+        name : "isfar",
+        email : "sazid@gmail.com"
+    }
+]
+
+
