176dac5f7b21d8fe43682f33bd94175347b5447a
Isfar Supayan Sazid <isfarsupayansazidpro25@gmail.com>
2024-06-17 16:29:42 +0000

    object

---
+++ 02.Basic/2.Object/script.js
@@ -0,0 +1,30 @@
+// const mySym = Symbol("key1")
+
+/* const JSuser = {
+    userName : "sazid",
+    "full Name" : "Isfar Supayan",
+    [mySym] : "key2",
+    userEmail : "isfar@google.com",
+    age : 22,
+    userId : 123456,
+    isLoggedIn : true,
+    lastLoginDays : ["Sunday","Monday"]
+} */
+// JSuser.userEmail = "sazid@google.com";
+// console.log(JSuser.userEmail);
+// console.log(JSuser["userEmail"]);
+// console.log(JSuser["full Name"]);
+// console.log(JSuser[mySym]);
+
+
+// Object.freeze(JSuser) // porer line gulo execute hobe na karon object freeze kora 
+// JSuser.userEmail = "sazid@google.com"
+// console.log(JSuser);
+
+
+/* JSuser.greeting = function(){
+    console.log(`Hello Js, ${this["full Name"]}`);
+};
+console.log(JSuser.greeting()); */
+
+
