097309e6c0156b6731b510738489503b6bd18edb
Isfar Supayan Sazid <isfarsupayansazidpro25@gmail.com>
2024-06-18 17:53:28 +0000

    object de-structure and API intro

---
+++ 02.Basic/3.ObjectandAPI/script.js
@@ -0,0 +1,43 @@
+const course = {
+    name : "Isfar",
+    price : "123",
+    courseInstruntor : "sazid"
+}
+
+// course.courseInstruntor;
+// console.log(course);
+// const {courseInstruntor} = course;
+// console.log(courseInstruntor);
+// const {courseInstruntor : instructor} = course
+// console.log(instructor);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// API introduction 
+// API always JSON Format and JSON can be object or array 
+
+/*{
+    "name" : "sazid",
+    "price" : 1234;
+    "email" : "sazid@google.com"
+}*/
+
+/*[
+    {
+    "name" : "sazid",
+    "price" : 1234;
+    "email" : "sazid@google.com"
+    }
+    {
+    "name" : "sazid",
+    "price" : 1234;
+    "email" : "sazid@google.com"
+    }
+    {
+    "name" : "sazid",
+    "price" : 1234;
+    "email" : "sazid@google.com"
+    }
+
+] */
+
+
